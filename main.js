e559f60cf93740d0568732821d7b437afcfa29fd
David Banham <david.banham@gmail.com>
2012-02-22 11:21:20 +1100

    Initial commit.
    Login and generic call function implemented

---
+++ main.js
@@ -0,0 +1,66 @@
+var request = require('request')
+	, crypto = require('crypto')
+	, qs = require('querystring')
+	, xml2js = require('xml2js')
+	, parser = new xml2js.Parser()
+	, endPoint = 'http://api4.int.liverail.com'
+	, liverail = exports
+	, token = undefined
+	;
+
+function setEndPoint(env) {
+	if ( env === 'prod' ) {
+		endPoint = 'http://api4.liverail.com'
+	} else {
+		endPoint = 'http://api4.int.liverail.com'
+	}
+}
+
+liverail.login = function (user, pass, callback) {
+	var passHash = crypto.createHash('md5').update(pass).digest('hex');
+	request({
+		method: 'POST',
+		uri: endPoint + '/login',
+		headers: {"content-type": "application/x-www-form-urlencoded"},
+		body: qs.encode({
+			username: user,
+			password: passHash
+		})
+	}, function (error, response, body) {
+		parser.parseString(body, function (err, result) {
+			token = result.auth.token;
+			if (typeof callback !== "undefined") {
+				callback(result);
+			}
+		});
+	})
+};
+
+liverail.call = function () {
+	var argArray = [];
+	for ( var i = 0 ; i < arguments.length ; i++ ) {
+		argArray.push(arguments[i]);
+	}
+	var callback = argArray.pop();
+	if ( token === undefined ) return callback('Login token not set')
+	var route = argArray.shift();
+	if ( typeof argArray[0] !== 'undefined' ) var input = argArray[0];
+	if ( typeof input === 'undefined' )	input = {};
+	input.token = token;
+	request({
+		method: 'POST',
+		uri: endPoint + route,
+		headers: {"content-type": "application/x-www-form-urlencoded"},
+		body: qs.encode(input)
+	}, function(error, response, body) {
+		if ( typeof callback !== 'undefined') {
+			if(error) {
+				return callback(error)
+			}
+			parser.parseString(body, function (error, result) {
+				if(error) return callback(error)
+				return callback(false, result, body, response)
+			});
+		}
+	});
+};
